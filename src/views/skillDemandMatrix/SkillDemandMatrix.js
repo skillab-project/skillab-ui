80028d23394972984474945873e0699f0b335913
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2025-02-10 10:08:22 +0200

    Skill demand matrix page

--- src/views/skillDemandMatrix/SkillDemandMatrix.js
+++ src/views/skillDemandMatrix/SkillDemandMatrix.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Line, Pie } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardHeader,
@@ -15,54 +14,92 @@ import {
   TabContent,
   TabPane
 } from "reactstrap";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,Legend,LineChart,Line} from "recharts";
 import classnames from 'classnames';
-import Overall from "./Overall";
-import PerOccupation from "./PerOccupation";
+import axios from 'axios';
+import GroupLevel from "./GroupLevel";
+import OccupationSelectionAndPillar from './OccupationSelectionAndPillar';
+import "../../assets/css/loader.css";
 
 
 function SkillDemandMatrix() {
-    const [currentActiveTab, setCurrentActiveTab] = useState('1');
-    const toggle = tab => {
-        if (currentActiveTab !== tab) setCurrentActiveTab(tab);
-    }
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
+    var dataSkills = [
+        { skill: "skill 1", priority: 30, rank: 20 },
+        { skill: "skill 2", priority: 40, rank: 25 },
+        { skill: "skill 3", priority: 25, rank: 30 },
+        { skill: "skill 4", priority: 35, rank: 20 },
+        { skill: "skill 5", priority: 45, rank: 15 },
+    ];
+    var selectedOccupations= ["skill","priority","rank"];
+    
+    
+
+    const handleApplyOccupationSelection = (selectedOccupation, selectedItem) => {
+        console.log('Occupation received:', selectedOccupation);
+        console.log('Skill category:', selectedItem);
+        var occupation = selectedOccupation.id;
+        var pillar = selectedItem;
+        setLoading(true);
+
+        axios
+            .get(process.env.REACT_APP_API_URL_SKILL_DEMAND_MATRIX + "/HierarchicalCumulativeVoting?url="+
+                                process.env.REACT_APP_API_URL_TRACKER + "/api/jobs" +
+                                "&pillar=" + pillar + "&occupation=" + occupation +
+                                "&source=OJA")
+            .then(async (res) => {
+                setLoading(false);
+                // Grouping data by level
+                const groupedData = res.data.reduce((acc, item) => {
+                    // Check if the level already exists in the accumulator
+                    const levelIndex = acc.findIndex((group) => group[0].level === item.level);
+            
+                    const simplifiedItem = {
+                        skill: item.skill,
+                        level: item.level,
+                        normalized_priority: item["normalized priority"],
+                        rank: item.rank,
+                    };
+            
+                    if (levelIndex !== -1) {
+                        acc[levelIndex].push(simplifiedItem);
+                    } else {
+                        acc.push([simplifiedItem]);
+                    }
+            
+                    return acc;
+                }, []);
+                console.log("Grouped Data:", groupedData);
+                
+                setData(groupedData);
+            })
+            .catch((err) => {
+              console.error("Error fetching data:", err);
+            });
+    }
     
     return (
         <div className="content">
-            <Nav tabs style={{marginBottom:"5px"}}>
-                <NavItem style={{cursor:"pointer"}}>
-                    <NavLink
-                        className={classnames({
-                            active:
-                                currentActiveTab === '1'
-                        })}
-                        onClick={() => { toggle('1'); }}
-                    >
-                        Overall
-                    </NavLink>
-                </NavItem>
-                <NavItem style={{cursor:"pointer"}}>
-                    <NavLink
-                        className={classnames({
-                            active:
-                                currentActiveTab === '2'
-                        })}
-                        onClick={() => { toggle('2'); }}
-                    >
-                        per Occupation
-                    </NavLink>
-                </NavItem>
-            </Nav>
+            <Row>
+                <Col md="12">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle tag="h5">Select occupation</CardTitle>
+                            <OccupationSelectionAndPillar onApplySelection={handleApplyOccupationSelection}/>
+                        </CardHeader>
+                        <CardBody>
+                            {loading && <div class="lds-dual-ring"></div>}
+                            {data.map((group) => (
+                                <GroupLevel data={group} />
+                            ))}
 
-            <TabContent activeTab={currentActiveTab}>
-                <TabPane tabId="1">
-                    <Overall />
-                </TabPane>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
 
-                <TabPane tabId="2">
-                    <PerOccupation />
-                </TabPane>
-            </TabContent>
         </div>
     );
 }
