f64154ffd5c748dc20496781b3edb48d91fe618f
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2025-03-11 14:08:47 +0200

    Course Info and selcection of Department

--- src/views/education/Courses.js
+++ src/views/education/Courses.js
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from "react";
 import {Card, CardHeader, CardBody, CardFooter, CardTitle, Row, Col, Button, Nav, NavItem, NavLink, TabContent, TabPane } from "reactstrap";
-
+import ProgramSelection from "./ProgramSelection.js"
 
 const Courses = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
+    const [search, setSearch] = useState(false);
+    const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
         //to DO
     }, []);
 
+    const handleApplyProgramSelection = (selectedDepartment, selectedProgram) => {
+        console.log('Deparment: ', selectedDepartment);
+        console.log('Program:', selectedProgram);
+    }
+
     return (
         <Row>
             <Col md="12">
                 <Card>
+                    <CardHeader>
+                        <CardTitle tag="h5">Select Program</CardTitle>
+                        <ProgramSelection onApplySelection={handleApplyProgramSelection}/>
+                    </CardHeader>
                     <CardBody>
-                        Courses
+                        {loading ? (
+                            <div className="lds-dual-ring"></div>
+                        ) : !search ? (
+                            <></>
+                        ) : (
+                            <>show</>
+                        )}
                     </CardBody>
                 </Card>
             </Col>
